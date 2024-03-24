ec5f12e8399c37c4c2457c143a1d3090cc78ce4a
Jonas Karlman <jonas@kwiboo.se>
2024-03-24 23:35:51 +0000

    TEMP: rockchip: add helios64 board

---
+++ include/configs/helios64.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2020 Aditya Prayoga <aditya@kobol.io>
+ */
+
+#ifndef __HELIOS64_H
+#define __HELIOS64_H
+
+#include <configs/rk3399_common.h>
+
+#endif
