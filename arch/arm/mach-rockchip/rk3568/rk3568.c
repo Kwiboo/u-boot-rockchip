74f1e6d2db8e930c9d294afb6f0dbdf33b93683f
Jonas Karlman <jonas@kwiboo.se>
2024-10-15 11:49:51 +0000

    WIP: rockchip: rk356x: Implement checkboard() to print SoC variant

--- arch/arm/mach-rockchip/rk3568/rk3568.c
+++ arch/arm/mach-rockchip/rk3568/rk3568.c
@@ -4,6 +4,7 @@
  */
 
 #include <dm.h>
+#include <misc.h>
 #include <asm/armv8/mmu.h>
 #include <asm/arch-rockchip/bootrom.h>
 #include <asm/arch-rockchip/grf_rk3568.h>
@@ -131,5 +132,64 @@ int arch_cpu_init(void)
 	writel(0x3f3f0707, GRF_BASE + GRF_GPIO1C_DS_2);
 	writel(0x3f3f0707, GRF_BASE + GRF_GPIO1C_DS_3);
 #endif
+	return 0;
+}
+
+#define RK3568_OTP_CPU_CODE_OFFSET		0x02
+#define RK3568_OTP_SPECIFICATION_OFFSET		0x07
+#define RK3568_OTP_CPU_VERSION_OFFSET		0x08
+#define RK3568_OTP_PERFORMANCE_OFFSET		0x22
+
+int checkboard(void)
+{
+	u8 cpu_code[2], specification, package, cpu_version, performance;
+	struct udevice *dev;
+	char suffix[3];
+	int ret;
+
+	ret = uclass_get_device_by_driver(UCLASS_MISC,
+					  DM_DRIVER_GET(rockchip_otp), &dev);
+	if (ret) {
+		debug("%s: could not find otp device, ret=%d\n", __func__, ret);
+		return 0;
+	}
+
+	ret = misc_read(dev, RK3568_OTP_CPU_CODE_OFFSET, cpu_code, 2);
+	if (ret < 0) {
+		debug("%s: could not read cpu-code, ret=%d\n", __func__, ret);
+		return 0;
+	}
+
+	ret = misc_read(dev, RK3568_OTP_SPECIFICATION_OFFSET, &specification, 1);
+	if (ret < 0) {
+		debug("%s: could not read specification, ret=%d\n", __func__, ret);
+		return 0;
+	}
+	package = specification >> 5;
+	specification &= 0x1f;
+
+	ret = misc_read(dev, RK3568_OTP_CPU_VERSION_OFFSET, &cpu_version, 1);
+	if (ret < 0) {
+		debug("%s: could not read cpu-version, ret=%d\n", __func__, ret);
+		return 0;
+	}
+	cpu_version >>= 3;
+	cpu_version &= 0x7;
+
+	ret = misc_read(dev, RK3568_OTP_PERFORMANCE_OFFSET, &performance, 1);
+	if (ret < 0) {
+		debug("%s: could not read performance, ret=%d\n", __func__, ret);
+		return 0;
+	}
+	if (performance & 0x0f)
+		specification = 0x14; // T
+
+	suffix[0] = specification > 1 ? '@' + specification : 0;
+	suffix[1] = package > 1 ? '0' + package : 0;
+	suffix[2] = 0;
+
+	printf("SoC:   RK%02x%02x%s (cpu-version: %x)\n",
+	       cpu_code[0], cpu_code[1], suffix, cpu_version);
+
 	return 0;
 }
