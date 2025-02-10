rpardini/armbian-patched-u-boot

board: rockchip: Add Pine64 PineNote
The PineNote is an e-ink tablet by PINE64. It is built around a Rockchip
RK3566 SoC and has the following specifications:
- 4GB LPDDR4
- 128GB eMMC
- 10.3" 1404x1872 e-ink display

This board is very similar to other RK356x-based boards, with the
exception that its download-mode "key" (actually a Hall sensor) is on
ADC channel 0 instead of 1.

As such, this patch is heavily based on the evb_3568 board
implementation with the addition of a simple board setup code, meant to
account for the above difference.

Signed-off-by: Arnaud Ferraris <arnaud.ferraris@collabora.com>
Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

board/pine64/pinenote-rk3566/pinenote-rk3566.c
@@ -1,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * (C) Copyright 2019 Rockchip Electronics Co., Ltd
+ * (C) Copyright 2023 Collabora Ltd
+ */
+
+#include <common.h>
+#include <adc.h>
+
+#define KEY_DOWN_MIN_VAL        0
+#define KEY_DOWN_MAX_VAL        30
+
+int rockchip_dnl_key_pressed(void)
+{
+	unsigned int val;
+
+	if (adc_channel_single_shot("saradc@fe720000", 0, &val)) {
+		printf("%s read adc key val failed\n", __func__);
+		return false;
+	}
+
+	if (val >= KEY_DOWN_MIN_VAL && val <= KEY_DOWN_MAX_VAL)
+		return true;
+	else
+		return false;
+}

Kwiboo/u-boot-rockchip

board/pine64/pinenote-rk3566/pinenote-rk3566.c
@@ -1,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * (C) Copyright 2019 Rockchip Electronics Co., Ltd
+ * (C) Copyright 2023 Collabora Ltd
+ */
+
+#include <adc.h>
+#include <stdio.h>
+
+#define KEY_DOWN_MIN_VAL        0
+#define KEY_DOWN_MAX_VAL        30
+
+int rockchip_dnl_key_pressed(void)
+{
+	unsigned int val;
+
+	if (adc_channel_single_shot("saradc@fe720000", 0, &val)) {
+		printf("%s read adc key val failed\n", __func__);
+		return false;
+	}
+
+	if (val >= KEY_DOWN_MIN_VAL && val <= KEY_DOWN_MAX_VAL)
+		return true;
+	else
+		return false;
+}