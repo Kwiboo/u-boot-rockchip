24e774206ea8ecc573affa3617e22d6ed0086fb5
Joseph Chen <chenjh@rock-chips.com>
2026-08-26 17:51:12 +0800

    power: Rename fg_test to charge_test
    Change-Id: I059bf4857b55e49af53ff6693d4937d89fe30bac
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

---
+++ drivers/power/charge_test.c
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 2025 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#include <dm.h>
+#include <common.h>
+#include <power/fuel_gauge.h>
+#include <power/charge_animation.h>
+
+static int charge_test_bat_is_exit(struct udevice *dev)
+{
+	return 1;
+}
+
+static int charge_test_get_soc(struct udevice *dev)
+{
+	return 50;
+}
+
+static int charge_test_get_voltage(struct udevice *dev)
+{
+	return 3600;
+}
+
+static int charge_test_get_current(struct udevice *dev)
+{
+	return 1000;
+}
+
+static bool charge_test_get_chrg_online(struct udevice *dev)
+{
+	return 1;
+}
+
+static int charge_test_charger_capability(struct udevice *dev)
+{
+	return FG_CAP_CHARGER | FG_CAP_FUEL_GAUGE;
+}
+
+static struct dm_fuel_gauge_ops charge_test_ops = {
+	.bat_is_exist = charge_test_bat_is_exit,
+	.get_soc = charge_test_get_soc,
+	.get_voltage = charge_test_get_voltage,
+	.get_current = charge_test_get_current,
+	.get_chrg_online = charge_test_get_chrg_online,
+	.capability = charge_test_charger_capability,
+};
+
+U_BOOT_DRIVER(charge_test) = {
+	.name = "charge_test",
+	.id = UCLASS_FG,
+	.ops = &charge_test_ops,
+};
+
+U_BOOT_DRVINFO(charge_test) = {
+	.name = "charge_test",
+};
+
+static const struct charge_animation_pdata ca_pdata = {
+	.uboot_charge = 1,
+	.android_charge = 0,
+	.low_power_voltage = 3350,
+	.screen_on_voltage = 3400,
+};
+
+U_BOOT_DRVINFO(charge_animation) = {
+	.name = "charge-animation",
+	.plat = &ca_pdata,
+};
+
